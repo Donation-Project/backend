cb81e4ac80b14546c7225e48f55efdb5236a5cce
jangwon3828 <wkd3828@gmail.com>
2022-10-18 10:33:11 +0900

    Wonjin_관리자 컨트롤러 포스트 상태에따른 전체조회 수정

--- src/main/java/com/donation/web/controller/Admin/AdminController.java
+++ src/main/java/com/donation/web/controller/Admin/AdminController.java
@@ -1,7 +1,6 @@
 package com.donation.web.controller.Admin;
 
 import com.donation.common.CommonResponse;
-import com.donation.common.response.post.PostFindRespDto;
 import com.donation.common.response.post.PostListRespDto;
 import com.donation.domain.enums.PostState;
 import com.donation.service.post.PostService;
@@ -9,23 +8,26 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api/Admin")
+@RequestMapping("/api/admin")
 public class AdminController {
 
     private final PostService postService;
 
-    @GetMapping
-    public ResponseEntity<?> getPostList(Pageable pageable) {
-        Slice<PostListRespDto> list = postService.getAdminPostList(pageable);
+    @GetMapping("/{state}")
+    public ResponseEntity<?> getPostList(Pageable pageable, @PathVariable PostState... state) {
+        log.info(state[0].toString()+state[1].toString());
+        Slice<PostListRespDto> list = postService.getAdminPostList(pageable,state);
         return ResponseEntity.ok(CommonResponse.success(list));
     }
+
+
+
 
 
 }
