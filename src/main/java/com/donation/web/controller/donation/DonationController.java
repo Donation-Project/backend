9eb028064dc91d1990103a6b063d98a999cc68bd
jangwon3828 <wkd3828@gmail.com>
2022-10-20 16:40:07 +0900

    Wonjin_후원 컨트롤러 : 내후원 내역조회

--- src/main/java/com/donation/web/controller/donation/DonationController.java
+++ src/main/java/com/donation/web/controller/donation/DonationController.java
@@ -2,6 +2,7 @@ package com.donation.web.controller.donation;
 
 import com.donation.common.CommonResponse;
 import com.donation.common.request.donation.DonationSaveReqDto;
+import com.donation.common.response.donation.DonationFindRespDto;
 import com.donation.service.donation.DonationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +27,12 @@ public class DonationController {
     ) {
         donationService.save(donationSaveReqDto);
         return new ResponseEntity<>(CommonResponse.success(), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable Long id) {
+        List<DonationFindRespDto> donationFindRespDtos = donationService.findById(id);
+        return ResponseEntity.ok(CommonResponse.success(donationFindRespDtos));
     }
 
 
