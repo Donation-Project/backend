679c6f48e0e6fa69269568a999b41e55a22ebd8e
WoojinDeve <jwj060111@gmail.com>
2022-11-04 18:26:49 +0900

    feat: 리프레쉬 토큰을 저장하는 TokenRepository 생성

---
+++ src/main/java/com/donation/auth/repository/TokenRepository.java
@@ -0,0 +1,14 @@
+package com.donation.auth.repository;
+
+public interface TokenRepository {
+
+    String save(final Long memberId, final String refreshToken);
+
+    void deleteAll();
+
+    void deleteByMemberId(final Long memberId);
+
+    boolean exist(final Long memberId);
+
+    String getToken(final Long memberId);
+}
