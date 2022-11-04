1989e637596a0717979423ce0d5110e4950210f9
WoojinDeve <jwj060111@gmail.com>
2022-11-04 19:10:14 +0900

    feat: AccessAndRefreshTokenResponse 코드 리팩토링

--- src/main/java/com/donation/common/response/auth/AccessAndRefreshTokenResponse.java
+++ src/main/java/com/donation/common/response/auth/AccessAndRefreshTokenResponse.java
@@ -1,5 +1,6 @@
 package com.donation.common.response.auth;
 
+import com.donation.service.auth.domain.AuthToken;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,13 @@ public class AccessAndRefreshTokenResponse {
     public AccessAndRefreshTokenResponse(final String accessToken, final String refreshToken) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+    }
+
+
+    public static AccessAndRefreshTokenResponse of(AuthToken authToken) {
+        return AccessAndRefreshTokenResponse.builder()
+                .accessToken(authToken.getAccessToken())
+                .refreshToken(authToken.getRefreshToken())
+                .build();
     }
 }
