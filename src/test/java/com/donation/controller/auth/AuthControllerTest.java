f57d0837e9af21f2915e5af5a7e3b26fcb9b5fe7
WoojinDeve <jwj060111@gmail.com>
2022-11-06 20:44:36 +0900

    feat: 리프레시 토큰이 유효하지 않을 경우 오류 검증 테스트

--- src/test/java/com/donation/controller/auth/AuthControllerTest.java
+++ src/test/java/com/donation/controller/auth/AuthControllerTest.java
@@ -1,6 +1,8 @@
 package com.donation.controller.auth;
 
+import com.donation.common.request.auth.TokenRenewalRequest;
 import com.donation.common.utils.ControllerTest;
+import com.donation.exception.DonationInvalidateException;
 import com.donation.service.auth.application.AuthService;
 import com.donation.web.controller.auth.AuthController;
 import org.junit.jupiter.api.DisplayName;
@@ -11,13 +13,14 @@ import org.springframework.restdocs.payload.JsonFieldType;
 
 import static com.donation.common.AuthFixtures.리프레쉬_토큰_발급_DTO;
 import static com.donation.common.AuthFixtures.리프레쉬_토큰_응답_DTO;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
@@ -47,8 +50,33 @@ public class AuthControllerTest extends ControllerTest {
                         )
                 ));
     }
-//
-//    @Test
-//    @DisplayName("리프레쉬 토큰이 유효하지 않을 경우 오류를 던진다.")
-//    void
+
+    @Test
+    @DisplayName("리프레쉬 토큰이 유효하지 않을 경우 오류를 던진다.")
+    void 리프레시_토큰이_유효하지_않을_경우_오류를_던진다() throws Exception {
+        //given
+        TokenRenewalRequest 잘못된_토큰_정보 = new TokenRenewalRequest("잘못된 토큰 정보");
+        given(authService.renewalToken(any())).willThrow(new DonationInvalidateException("권한이 없습니다."));
+
+        //expected
+        mockMvc.perform(post("/api/auth/token/access")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(잘못된_토큰_정보)))
+                .andExpect(status().isBadRequest())
+                .andDo(print())
+                .andDo(document("auth-fail-refreshToken",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("refreshToken").type(JsonFieldType.STRING)
+                                        .description("잘못된 토큰 정보")
+                        ),
+                        responseFields(
+                                fieldWithPath("success").description("성공 여부"),
+                                fieldWithPath("data").description("성공시 데이터 반환"),
+                                fieldWithPath("error.errorCode").description("BAD_REQUEST"),
+                                fieldWithPath("error.errorMessage").description("권한이 없습니다.")
+                        )
+                ));
+    }
 }
