b3dc00efe9bbd6e746b6a0087dca969add2458a6
WoojinDeve <jwj060111@gmail.com>
2022-11-08 20:41:46 +0900

    feat: 비밀번호 변경 요청 서비스 로직 테스트

--- src/test/java/com/donation/service/user/UserServiceTest.java
+++ src/test/java/com/donation/service/user/UserServiceTest.java
@@ -1,14 +1,16 @@
 package com.donation.service.user;
 
 import com.donation.auth.LoginMember;
+import com.donation.common.request.user.UserLoginReqDto;
 import com.donation.common.response.user.UserEmailRespDto;
 import com.donation.common.response.user.UserRespDto;
 import com.donation.common.utils.ServiceTest;
 import com.donation.domain.entites.User;
+import com.donation.exception.DonationInvalidateException;
 import com.donation.repository.user.UserRepository;
 import com.donation.repository.utils.PageCustom;
+import com.donation.service.auth.application.AuthService;
 import com.donation.service.s3.AwsS3Service;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,8 +20,12 @@ import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
+import static com.donation.common.AuthFixtures.회원검증;
 import static com.donation.common.UserFixtures.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 
 class UserServiceTest extends ServiceTest {
@@ -31,9 +37,12 @@ class UserServiceTest extends ServiceTest {
     @Autowired
     private AwsS3Service awsS3Service;
 
+    @Autowired
+    private AuthService authService;
+
     @Test
     @DisplayName("20명의 회원을 10개씩 페이징후 조회")
-    void 회원20명의_정보를_10명씩_페이징_조회(){
+    void 회원20명의_정보를_10명씩_페이징_조회() {
         //given
         List<User> users = creatUserList(0, 20);
         userRepository.saveAll(users);
@@ -42,7 +51,7 @@ class UserServiceTest extends ServiceTest {
         PageCustom<UserRespDto> actual = userService.getList(PageRequest.of(0, 10));
 
         //then
-        Assertions.assertAll(() -> {
+        assertAll(() -> {
             assertThat(actual.getPage().getTotalElement()).isEqualTo(20);
             assertThat(actual.getPage().getTotalPages()).isEqualTo(2);
             assertThat(actual.getContent().size()).isEqualTo(10);
@@ -52,7 +61,7 @@ class UserServiceTest extends ServiceTest {
 
     @Test
     @DisplayName("저장된 회원 새로운 프로필 등록")
-    void 저장된_회원_새로운_프로필_등록(){
+    void 저장된_회원_새로운_프로필_등록() {
         //given
         User user = userRepository.save(createUser());
         LoginMember loginMember = new LoginMember(user.getId());
@@ -84,7 +93,7 @@ class UserServiceTest extends ServiceTest {
 
     @Test
     @DisplayName("이메일이 존재한다면 (이미 중복된 이메일이 존재합니다.)메시지를 전달한다")
-    void 이메일이_존재한다면_이미_중복된_이메일이_존재합니다_메시지를_전달한다(){
+    void 이메일이_존재한다면_이미_중복된_이메일이_존재합니다_메시지를_전달한다() {
         //given
         User user = userRepository.save(createUser());
 
@@ -99,11 +108,37 @@ class UserServiceTest extends ServiceTest {
     @ParameterizedTest
     @DisplayName("이메일이 중복되지않는다면 (사용가능한 이메일입니다.)메시지를 전달한다")
     @ValueSource(strings = {"default@email.com"})
-    void 이메일이_중복되지않는다면_사용가능한_이메일입니다_메시지를_전달한다(final String email){
+    void 이메일이_중복되지않는다면_사용가능한_이메일입니다_메시지를_전달한다(final String email) {
         //given & when
         UserEmailRespDto userEmailRespDto = userService.checkUniqueEmail(email);
 
         //then
         assertThat(userEmailRespDto.getMessage()).isEqualTo("사용가능한 이메일입니다.");
+    }
+
+    @Test
+    @DisplayName("사용자의 비밀번호를 변경합니다.")
+    void 사용자의_비밀번호를_변경합니다() {
+        //given
+        Long id = authService.save(유저_회원가입_DTO);
+
+        //when
+        userService.passwordModify(회원검증(id), 비밀번호_변경_DTO(일반_사용자_패스워드, 새로운_일반_사용자_패스워드));
+
+        //then
+        assertDoesNotThrow(() -> authService.login(new UserLoginReqDto(일반_사용자_이메일, 새로운_일반_사용자_패스워드)));
+    }
+
+    @ParameterizedTest
+    @DisplayName("사용자의 비밀번호를 변경할때 현재비밀번호가 일치하지않으면 예외를 던진다.")
+    @ValueSource(strings = {"잘못된 현재 비밀번호"})
+    void 사용자의_비밀번호를_변경할때_현재_비밀번호가_일치하지않으면_예외를_던진다(final String 잘못된비밀번호) {
+        //given
+        Long id = authService.save(유저_회원가입_DTO);
+
+        //when & then
+        assertThatThrownBy(() -> userService.passwordModify(회원검증(id), 비밀번호_변경_DTO(잘못된비밀번호, 새로운_일반_사용자_패스워드)))
+                .isInstanceOf(DonationInvalidateException.class)
+                .hasMessage("패스워드가 일치하지 않습니다.");
     }
 }
