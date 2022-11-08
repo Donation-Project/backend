7b5e98c8e673d1b7fac70cfd97b6385f43c986ac
WoojinDeve <jwj060111@gmail.com>
2022-11-08 20:14:36 +0900

    feat: 이메일 중복검사 컨트롤러 로직 테스트

--- src/test/java/com/donation/controller/user/UserControllerTest.java
+++ src/test/java/com/donation/controller/user/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.donation.controller.user;
 
 import com.donation.auth.LoginMember;
 import com.donation.common.UserFixtures;
+import com.donation.common.request.user.UserEmailReqDto;
+import com.donation.common.response.user.UserEmailRespDto;
 import com.donation.common.response.user.UserRespDto;
 import com.donation.common.utils.ControllerTest;
 import com.donation.repository.utils.PageCustom;
@@ -90,6 +92,27 @@ class UserControllerTest extends ControllerTest {
     }
 
     @Test
+    @DisplayName("이메일 중복확인 요청 성공(중복되지 않은 이메일)")
+    void 이메일_중복확인_요청_성공() throws Exception {
+        //given
+        given(userService.checkUniqueEmail(일반_사용자_이메일)).willReturn(UserEmailRespDto.of(false));
+
+        //expected
+        mockMvc.perform(post("/api/join/exists")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new UserEmailReqDto(일반_사용자_이메일)))
+                )
+                .andDo(print())
+                .andDo(document("user-email",
+                        preprocessRequest(),
+                        preprocessResponse(),
+                        requestFields(
+                                fieldWithPath("email").description("중복 확인 요청 이메일")
+                        ))
+                );
+    }
+
+    @Test
     @DisplayName("회원의 ID를 통한 회원단건조회 요청 성공")
     void 회원의_ID를_통한_회원단건조회_요청_성공() throws Exception {
         //given
@@ -121,7 +144,7 @@ class UserControllerTest extends ControllerTest {
 
     @Test
     @DisplayName("회원 리스트 조회시 10개씩 페이징되어 조회된다.")
-    void 회원_리스트_조회시_10개씩_페이징되어_조회된다() throws Exception{
+    void 회원_리스트_조회시_10개씩_페이징되어_조회된다() throws Exception {
         //given
         List<UserRespDto> contet = LongStream.range(1, 11).mapToObj(UserFixtures::일반_반환_데이터).collect(Collectors.toList());
         PageCustom<UserRespDto> response = new PageCustom<>(PageableExecutionUtils.getPage(contet, PageRequest.of(0, 10), () -> 20L));
