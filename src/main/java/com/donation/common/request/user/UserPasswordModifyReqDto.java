2dee10900013a3d6467b1659d9194d1b8299fb5e
WoojinDeve <jwj060111@gmail.com>
2022-11-08 20:46:44 +0900

    feat: 비밀번호 변경 입력 DTO에 Validate 어노테이션 추가

--- src/main/java/com/donation/common/request/user/UserPasswordModifyReqDto.java
+++ src/main/java/com/donation/common/request/user/UserPasswordModifyReqDto.java
@@ -3,11 +3,15 @@ package com.donation.common.request.user;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class UserPasswordModifyReqDto {
 
+    @NotBlank
     private String currentPassword;
+    @NotBlank
     private String modifyPassword;
 
     public UserPasswordModifyReqDto(final String currentPassword,final String modifyPassword) {
