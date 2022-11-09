c7060cfd1d97562f701a54feb28949cbf73816a5
WoojinDeve <jwj060111@gmail.com>
2022-11-09 12:16:38 +0900

    feat: 패스워드 설정 코드 분리

---
+++ src/main/java/com/donation/infrastructure/PasswordEncoder.java
@@ -0,0 +1,19 @@
+package com.donation.infrastructure;
+
+import com.donation.exception.DonationInvalidateException;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoder {
+
+    public String encode(final String password){
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public boolean compare(final String password, final String encodePassword){
+        if (!BCrypt.checkpw(password, encodePassword))
+            throw new DonationInvalidateException("패스워드가 일치하지 않습니다.");
+        return true;
+    }
+}
