f0b0aca27870404f3552f6779815e2251e6a57c6
WoojinDeve <jwj060111@gmail.com>
2022-11-11 14:23:58 +0900

    feat: Review 레포지토리 추가

---
+++ src/main/java/com/donation/domain/reviews/repository/ReviewRepository.java
@@ -0,0 +1,24 @@
+package com.donation.domain.reviews.repository;
+
+import com.donation.domain.reviews.entity.Reviews;
+import com.donation.global.exception.DonationDuplicateException;
+import com.donation.global.exception.DonationNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReviewRepository extends JpaRepository<Reviews, Long> {
+
+    Optional<Reviews> findByPostId(Long postId);
+
+    default Reviews getByPostId(Long postId){
+        return findByPostId(postId)
+                .orElseThrow(() -> new DonationNotFoundException("해당 검색결과로 존재햐는 글이 없습니다."));
+    }
+    boolean existsByPostId(Long postId);
+
+    default void validateExistsPostId(Long postId){
+        if(existsByPostId(postId))
+            throw new DonationDuplicateException("이미 작성한 글이 있습니다.");
+    }
+}
