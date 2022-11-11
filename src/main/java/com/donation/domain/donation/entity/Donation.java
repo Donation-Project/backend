af7d4448dd8e1ef9470675f99deb7159db013e4c
jangwon3828 <wkd3828@gmail.com>
2022-11-11 15:06:31 +0900

    refactor: 후원액 검증코드 추가

--- src/main/java/com/donation/domain/donation/entity/Donation.java
+++ src/main/java/com/donation/domain/donation/entity/Donation.java
@@ -1,5 +1,6 @@
 package com.donation.domain.donation.entity;
 
+import com.donation.global.exception.DonationInvalidateException;
 import com.donation.infrastructure.embed.BaseEntity;
 import com.donation.domain.post.entity.Post;
 import com.donation.domain.user.entity.User;
@@ -18,6 +19,9 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 @Getter
 public class Donation extends BaseEntity {
+    public static Float maxAmount=1000000.0F;
+    public static Float minAmount=0.0F;
+
     @Id @GeneratedValue(strategy = IDENTITY)
     @Column(name = "donationId")
     private Long id;
@@ -34,6 +38,7 @@ public class Donation extends BaseEntity {
 
     @Builder
     public Donation(Long id, User user, Post post,String amount) {
+        String validateAmount = validate(amount);
         this.id = id;
         this.user = user;
         this.post = post;
@@ -41,13 +46,24 @@ public class Donation extends BaseEntity {
     }
 
     public static Donation of(User user, Post post, String amount){
+        String validateAmount = validate(amount);
         return Donation.builder()
                 .user(user)
                 .post(post)
-                .amount(amount)
+                .amount(validateAmount)
                 .build();
     }
+
     public static Float toFloat(String amount){
         return Float.parseFloat(amount);
+    }
+    public static String validate(String amount){
+        Float Amount = toFloat(amount);
+        if(Amount>minAmount&&Amount<=maxAmount){
+            return Amount.toString();
+        }else {
+            throw new DonationInvalidateException(String.format("후원금액은 %.1f 보다크고 %.1f보다 작아야합니다",minAmount,maxAmount) );
+
+        }
     }
 }
